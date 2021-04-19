60be123ad2982dfa573a783a5c2a5cce507a99f6
Jesse Feng <fxm0016@126.com>
2021-04-19 23:11:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const Benchmark = require('benchmark');
-const { readdirSync } = require('fs');
-const { join } = require('path');
-// const execa = require('execa');
+const {join, isAbsolute} = require('path');
+const globby = require('globby');
 
-// const { stdout, stderr } = execa.commandSync('npm run build', { cwd: join(__dirname, '../') });
-// if (stdout) {
-//   console.log(stdout);
-// }
-// if (stderr) {
-//   console.error(stderr);
-// }
+function run(input) {
+  console.log('======== Benchmark start ========\n');
 
-console.log('======== 性能测试开始 ========\n');
+  const cwd = process.cwd();
+  const files = globby.sync(input);
+  for (let file of files) {
+    if (!isAbsolute(file)) {
+      file = join(cwd, file);
+    }
+    const tests = require(file);
+    const suite = new Benchmark.Suite({minSamples: 100});
+    Object.keys(tests).forEach(key => {
+      suite.add(key, tests[key]);
+    });
+    suite
+      .on('cycle', e => {
+        console.log(String(e.target));
+      })
+      .on('complete', e => {
+        console.log(`The fastest is ${e.currentTarget.filter('fastest').map('name')}\n`);
+      })
+      .run();
+  }
 
-readdirSync(join(__dirname, 'test')).forEach((file) => {
-  const tests = require(join(__dirname, 'test', file));
-  const suite = new Benchmark.Suite({ minSamples: 100 });
-  Object.keys(tests).forEach((key) => {
-    suite.add(key, tests[key]);
-  });
-  suite
-    .on('cycle', (e) => {
-      console.log(String(e.target));
-    })
-    .on('complete', (e) => {
-      console.log(`The fastest is ${e.currentTarget.filter('fastest').map('name')}\n`);
-    })
-    .run();
-});
+  console.log('======== Benchmark end ========');
 
-console.log('======== 性能测试结束 ========');
+}
+
+module.exports = run;
